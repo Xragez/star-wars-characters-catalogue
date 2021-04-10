94c36f05806ebcf8f2c3fc9b21d3c1f90756acae
pawel <pawelzeg@gmail.com>
2021-04-10 13:58:36 +0200

    Create App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import List from './components/list/List'
+import Title from './components/title/Title'
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Title size='large'>Star Wars Characters Catalogue</Title>
+      <List/>
     </div>
   );
 }
