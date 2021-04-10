01833b1a32f2550395b6d38ad4fa2704c9db22b9
pawel <pawelzeg@gmail.com>
2021-04-10 13:31:57 +0200

    Add card component

---
+++ src/components/card/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Card.module.css'
+
+
+function Card(props){
+  
+  return (
+    <div className={styles.cardContainer} onClick={props.onClick}>
+      {props.children}
+    </div>
+  )
+}
+
+export default Card
