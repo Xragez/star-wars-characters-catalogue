f254e1aacdc32fd3cc0cd83ad01dfbc1c3c5e035
pawel <pawelzeg@gmail.com>
2021-04-10 13:41:29 +0200

    Add propTypes

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styles from './Button.module.css'
 
+
+const propTypes = {
+  message: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+}
 
 function Button(props){
   const message = props.message
@@ -13,5 +19,7 @@ function Button(props){
     </button>
   )
 }
+
+Button.propTypes = propTypes
 
 export default Button
